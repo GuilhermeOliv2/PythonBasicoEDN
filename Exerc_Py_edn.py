33f540d54a83c789a3b3761ff1cb7f2c157e7cd1
GuilhermeOliv2 <oliveirag1920@gmail.com>
2025-05-05 11:08:07 -0300

    commit teste

---
+++ Exerc_Py_edn.py
@@ -0,0 +1,42 @@
+#Saudações
+print ("hello world!")
+print ("")
+print ("--------------------")
+print ("")
+#Soma
+numero1 = 12
+numero2 = 14
+
+soma = numero1 + numero2
+
+print ("a soma de", numero1, "+", numero2, "é", soma)
+
+print ("")
+print ("--------------------")
+print ("")
+
+#Calculo de volume
+
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print ("O volume de um solido com", comprimento,"cm de comprimento,", largura,"cm de largura e", altura,"cm de altura, é", volume,"cm³.")
+
+print ("")
+print ("--------------------")
+print ("")
+
+#calculadora de preço total
+
+nome_produto = "Cadeira infantil"
+preço_unit = 12.4
+qauntidade = 3
+preço_total = preço_unit * qauntidade
+
+print ("nome do produto:", nome_produto,
+       "\nnome do produto:", preço_unit,
+       "\nquantidade:", qauntidade,
+       "\npreço total:", preço_total)
